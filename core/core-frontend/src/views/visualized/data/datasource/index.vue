91a75653b80e37fd0188ba42ef7ef44b74ce04f4
dataeaseShu <tong.shu@fit2cloud.com>
2024-10-28 17:44:41 +0800

    fix(数据源): 数据源列表缺少状态显示

--- core/core-frontend/src/views/visualized/data/datasource/index.vue
+++ core/core-frontend/src/views/visualized/data/datasource/index.vue
@@ -6,6 +6,7 @@ import icon_dataset from '@/assets/svg/icon_dataset.svg'
 import icon_deleteTrash_outlined from '@/assets/svg/icon_delete-trash_outlined.svg'
 import icon_intoItem_outlined from '@/assets/svg/icon_into-item_outlined.svg'
 import icon_rename_outlined from '@/assets/svg/icon_rename_outlined.svg'
+import icon_warning_colorful_red from '@/assets/svg/icon_warning_colorful_red.svg'
 import dvFolder from '@/assets/svg/dv-folder.svg'
 import dvNewFolder from '@/assets/svg/dv-new-folder.svg'
 import icon_fileAdd_outlined from '@/assets/svg/icon_file-add_outlined.svg'
@@ -1084,6 +1085,12 @@ const getMenuList = (val: boolean) => {
                   <Icon :static-content="getDsIcon(data)"
                     ><component class="svg-icon" :is="getDsIconName(data)"></component
                   ></Icon>
+                </el-icon>
+                <el-icon
+                  style="position: absolute; top: 21px; left: 34px; font-size: 12px"
+                  v-if="data.extraFlag <= -1"
+                >
+                  <Icon><icon_warning_colorful_red class="svg-icon" /></Icon>
                 </el-icon>
                 <span
                   :title="node.label"
