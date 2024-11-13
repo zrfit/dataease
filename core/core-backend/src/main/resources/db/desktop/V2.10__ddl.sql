221a7caeeece4a74bd174b550b698bba1130d7b2
junjun <junjie.xia@fit2cloud.com>
2024-11-13 13:49:22 +0800

    fix: 修复异常SQL

--- core/core-backend/src/main/resources/db/desktop/V2.10__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.10__ddl.sql
@@ -22,4 +22,20 @@ alter table `core_chart_view`
     add ext_color longtext comment '颜色维度field';
 
 update visualization_outer_params_target_view_info set target_ds_id = (select ccv.table_id from visualization_outer_params_target_view_info tvi INNER JOIN core_chart_view ccv on tvi.target_view_id = ccv.id)
-where target_view_id = (select ccv.id from visualization_outer_params_target_view_info tvi INNER JOIN core_chart_view ccv on tvi.target_view_id = ccv.id)
+where target_view_id = (select ccv.id from visualization_outer_params_target_view_info tvi INNER JOIN core_chart_view ccv on tvi.target_view_id = ccv.id);
+
+DROP TABLE IF EXISTS `core_font`;
+CREATE TABLE `core_font`
+(
+    `id`                bigint       NOT NULL COMMENT 'ID',
+    `name`              varchar(255) NOT NULL COMMENT '字体名称',
+    `file_name`         varchar(255) default NULL COMMENT '文件名称',
+    `file_trans_name`   varchar(255) default NULL COMMENT '文件转换名称',
+    `is_default`         tinyint(1)       default 0 COMMENT '是否默认',
+    `update_time`        bigint     NOT NULL COMMENT '更新时间',
+    `is_BuiltIn`         tinyint(1)       default 0 COMMENT '是否内置',
+    PRIMARY KEY (`id`)
+);
+ALTER TABLE  `core_font` ADD COLUMN `size` DOUBLE NULL AFTER `is_BuiltIn`;
+ALTER TABLE  `core_font` ADD COLUMN `size_type` varchar(255) NULL AFTER `size`;
+INSERT INTO  `core_font` (`id`, `name`, `is_default`, `update_time`, `is_BuiltIn`) VALUES ('1', 'PingFang', '1', '0', '1');
