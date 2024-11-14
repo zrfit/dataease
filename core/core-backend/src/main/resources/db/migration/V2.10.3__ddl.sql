ea82b92c54c2d00132562efcc6493e94a0c5ff83
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-11-14 14:44:26 +0800

    feat: 增加消息中心菜单

--- core/core-backend/src/main/resources/db/migration/V2.10.3__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10.3__ddl.sql
@@ -1,2 +1,5 @@
 INSERT INTO `core_sys_setting`(`id`, `pkey`, `pval`, `type`, `sort`)
 VALUES (1048232869488627719, 'basic.defaultSort', '1', 'text', 13);
+
+INSERT INTO `core_menu` VALUES (70, 0, 1, 'msg', NULL, 200, NULL, '/msg', 1, 1, 0);
+INSERT INTO `core_menu` VALUES (71, 70, 2, 'msg-fill', 'msg/fill', 1, 'icon_msg_fill', '/msg-fill', 0, 1, 0);
