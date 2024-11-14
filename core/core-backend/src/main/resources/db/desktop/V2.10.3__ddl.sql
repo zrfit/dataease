17504dc0aeba6b1ac76ef2a1195f1374a03c73fc
taojinlong <jinlong@fit2cloud.com>
2024-11-14 18:13:51 +0800

    fix: 回滚

--- core/core-backend/src/main/resources/db/desktop/V2.10.3__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.10.3__ddl.sql
@@ -1,4 +1,2 @@
 INSERT INTO `core_sys_setting`(`id`, `pkey`, `pval`, `type`, `sort`)
 VALUES (1048232869488627719, 'basic.defaultSort', '1', 'text', 13);
-
-UPDATE `per_sys_setting` SET `pval` = 'true' WHERE (`pkey` = 'basic.dip');
