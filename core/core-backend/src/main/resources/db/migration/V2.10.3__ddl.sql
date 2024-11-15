f732b290db793e92889dfb0484f737a5570824c9
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-11-15 10:30:08 +0800

    perf(X-Pack): CAS和OIDC插件自动化配置

--- core/core-backend/src/main/resources/db/migration/V2.10.3__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10.3__ddl.sql
@@ -3,3 +3,5 @@ VALUES (1048232869488627719, 'basic.defaultSort', '1', 'text', 13);
 
 INSERT INTO `core_menu` VALUES (70, 0, 1, 'msg', NULL, 200, NULL, '/msg', 1, 1, 0);
 INSERT INTO `core_menu` VALUES (71, 70, 2, 'msg-fill', 'msg/fill', 1, 'icon_msg_fill', '/msg-fill', 0, 1, 0);
+
+UPDATE `xpack_setting_authentication` set `synced` = 0 where `name` = 'oidc' or name = 'cas';
--- core/core-frontend/src/views/data-visualization/PreviewHead.vue
+++ core/core-frontend/src/views/data-visualization/PreviewHead.vue
@@ -29,12 +29,7 @@ const embeddedStore = useEmbedded()
 
 const favorited = ref(false)
 const preview = () => {
-  const href = window.location.href
-  let baseUrl = isDataEaseBi.value ? embeddedStore.baseUrl : href.substring(0, href.indexOf('#'))
-  if (baseUrl.includes('oidcbi/') || baseUrl.includes('casbi/')) {
-    baseUrl = baseUrl.replace('oidcbi/', '')
-    baseUrl = baseUrl.replace('casbi/', '')
-  }
+  const baseUrl = isDataEaseBi.value ? embeddedStore.baseUrl : ''
   const url = baseUrl + '#/preview?dvId=' + dvInfo.value.id + '&ignoreParams=true'
   const newWindow = window.open(url, '_blank')
   initOpenHandler(newWindow)
