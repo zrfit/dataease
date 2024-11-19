f5e3d65c4b269cc6127427bdbffe7a804351412e
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-11-19 18:24:48 +0800

    feat(X-Pack): 增加游离资源管理菜单

--- core/core-backend/src/main/java/io/dataease/menu/manage/MenuManage.java
+++ core/core-backend/src/main/java/io/dataease/menu/manage/MenuManage.java
@@ -105,6 +105,7 @@ public class MenuManage {
                 || coreMenu.getId().equals(50L)
                 || coreMenu.getId().equals(60L)
                 || coreMenu.getId().equals(61L)
+                || coreMenu.getId().equals(80L)
                 || coreMenu.getPid().equals(70L);
     }
 }
