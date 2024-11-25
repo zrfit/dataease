03bce59153bdc3d5fca5bb93a550743e5ec7e8f3
ulleo <ulleo.wang@gmail.com>
2024-11-25 16:56:35 +0800

    feat(仪表板): 编辑仪表板出现数据异常时，给出详细的明确的报错信息
    #12745

--- core/core-frontend/src/views/chart/components/views/components/ChartError.vue
+++ core/core-frontend/src/views/chart/components/views/components/ChartError.vue
@@ -1,5 +1,6 @@
 <script lang="tsx" setup>
 import { useI18n } from '@/hooks/web/useI18n'
+import { ref } from 'vue'
 
 const { t } = useI18n()
 
@@ -10,11 +11,38 @@ const props = defineProps({
     default: ''
   }
 })
+
+const dialogVisible = ref(false)
+function showInfo() {
+  dialogVisible.value = true
+}
 </script>
 
 <template>
   <div class="canvas-content error-info">
-    <span>[{{ t('chart.chart_error_tips') }}]</span>
+    <span
+      >[{{ t('chart.chart_error_tips')
+      }}<a style="color: #0969da; cursor: pointer" @click="showInfo">{{
+        t('chart.chart_show_error_info')
+      }}</a
+      >]</span
+    >
+    <el-dialog
+      :append-to-body="true"
+      v-model="dialogVisible"
+      width="80%"
+      :close-on-click-modal="false"
+      center
+    >
+      <el-main style="height: 400px">
+        <span style="white-space: pre-line" v-html="errMsg"></span>
+      </el-main>
+      <template #footer>
+        <span class="m-dialog-footer">
+          <el-button @click="dialogVisible = false">{{ t('commons.close') }}</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -29,5 +57,11 @@ const props = defineProps({
   width: 100%;
   height: 100%;
   flex-direction: column;
+}
+.m-dialog-footer {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 </style>
