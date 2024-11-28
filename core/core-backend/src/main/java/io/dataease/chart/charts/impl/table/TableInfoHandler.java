d38f20fd55d8e280dd43c8b4af524712bb7ca3c6
taojinlong <jinlong@fit2cloud.com>
2024-11-28 18:25:34 +0800

    fix: 下载明细表实现分页

--- core/core-backend/src/main/java/io/dataease/chart/charts/impl/table/TableInfoHandler.java
+++ core/core-backend/src/main/java/io/dataease/chart/charts/impl/table/TableInfoHandler.java
@@ -44,7 +44,7 @@ public class TableInfoHandler extends DefaultChartHandler {
         Map<String, Object> mapSize = (Map<String, Object>) mapAttr.get("basicStyle");
         var tablePageMode = (String) mapSize.get("tablePageMode");
         formatResult.getContext().put("tablePageMode", tablePageMode);
-        if (StringUtils.equalsIgnoreCase(tablePageMode, "page") && !view.getIsExcelExport()) {
+        if (StringUtils.equalsIgnoreCase(tablePageMode, "page")) {
             if (chartExtRequest.getGoPage() == null) {
                 chartExtRequest.setGoPage(1L);
             }
@@ -127,6 +127,7 @@ public class TableInfoHandler extends DefaultChartHandler {
         querySql = provider.rebuildSQL(querySql, sqlMeta, crossDs, dsMap);
         datasourceRequest.setQuery(querySql);
         logger.debug("calcite chart sql: " + querySql);
+        System.out.println(querySql);
         List<String[]> data = (List<String[]>) provider.fetchResultField(datasourceRequest).get("data");
         //自定义排序
         data = ChartDataUtil.resultCustomSort(xAxis, data);
