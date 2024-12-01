1e1552952520361067547d503067273f0324ddb6
wangjiahao <1522128093@qq.com>
2024-12-01 20:53:27 +0800

    fix(工作台): 修复社区版最近使用资源不显示创建人名称，显示的是id问题

--- core/core-backend/src/main/java/io/dataease/visualization/server/VisualizationStoreServer.java
+++ core/core-backend/src/main/java/io/dataease/visualization/server/VisualizationStoreServer.java
@@ -4,9 +4,12 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.dataease.api.visualization.VisualizationStoreApi;
 import io.dataease.api.visualization.request.VisualizationStoreRequest;
 import io.dataease.api.visualization.request.VisualizationWorkbranchQueryRequest;
+import io.dataease.api.visualization.vo.VisualizationResourceVO;
 import io.dataease.api.visualization.vo.VisualizationStoreVO;
 import io.dataease.visualization.manage.VisualizationStoreManage;
 import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,7 @@ public class VisualizationStoreServer implements VisualizationStoreApi {
 
     @Resource
     private VisualizationStoreManage visualizationStoreManage;
+
     @Override
     public void execute(VisualizationStoreRequest request) {
         visualizationStoreManage.execute(request);
@@ -26,6 +30,13 @@ public class VisualizationStoreServer implements VisualizationStoreApi {
     @Override
     public List<VisualizationStoreVO> query(VisualizationWorkbranchQueryRequest request) {
         IPage<VisualizationStoreVO> iPage = visualizationStoreManage.query(1, 20, request);
+        List<VisualizationStoreVO> resourceVOS = iPage.getRecords();
+        if (!CollectionUtils.isEmpty(resourceVOS)) {
+            resourceVOS.stream().forEach(item -> {
+                item.setCreator(StringUtils.equals(item.getCreator(), "1") ? item.getCreator() : "管理员");
+                item.setLastEditor(StringUtils.equals(item.getLastEditor(), "1") ? item.getLastEditor() : "管理员");
+            });
+        }
         return iPage.getRecords();
     }
 
