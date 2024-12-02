822de6f2b82d026d81b6cafef7fc48625c0ef836
taojinlong <jinlong@fit2cloud.com>
2024-12-02 12:56:15 +0800

    fix:【数据源】MySQL 表数据中含有换行符时，查询组件会查不出来数据

--- sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/ChartExtFilterDTO.java
+++ sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/ChartExtFilterDTO.java
@@ -1,10 +1,13 @@
 package io.dataease.extensions.view.dto;
 
 
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.dataease.extensions.datasource.dto.DatasetTableFieldDTO;
 import lombok.Data;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -23,4 +26,16 @@ public class ChartExtFilterDTO {
     @JsonIgnore
     private List<String> originValue;
     private int filterType;// 0-过滤组件，1-下钻，2-联动，外部参数
+
+
+    public List<String> getValue() {
+        if (CollectionUtils.isNotEmpty(value)) {
+            List<String> values = new ArrayList<>();
+            value.forEach(v -> {
+                values.add(v.replace("\n", "\\n"));
+            });
+            return values;
+        }
+        return value;
+    }
 }
