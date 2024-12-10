709c4823e1b474de23cb66995501f03539bb1cae
taojinlong <jinlong@fit2cloud.com>
2024-12-10 18:23:39 +0800

    fix: 优化查询数据表最新更新时间

--- core/core-backend/src/main/java/io/dataease/job/schedule/CleanScheduler.java
+++ core/core-backend/src/main/java/io/dataease/job/schedule/CleanScheduler.java
@@ -1,5 +1,6 @@
 package io.dataease.job.schedule;
 
+import io.dataease.datasource.server.DatasourceTaskServer;
 import io.dataease.exportCenter.manage.ExportCenterManage;
 import io.dataease.utils.LogUtil;
 import jakarta.annotation.Resource;
@@ -11,11 +12,20 @@ public class CleanScheduler {
 
     @Resource(name = "exportCenterManage")
     private ExportCenterManage exportCenterManage;
+    @Resource(name = "datasourceTaskServer")
+    private DatasourceTaskServer datasourceTaskServer;
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void clean() {
         LogUtil.info("Start to execute export file cleaner ...");
         exportCenterManage.cleanLog();
         LogUtil.info("Execute export file cleaner success");
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void cleanSyncLog() {
+        LogUtil.info("Start to clean sync log ...");
+        datasourceTaskServer.cleanLog();
+        LogUtil.info("End to clean sync log.");
     }
 }
---
+++ core/core-backend/src/main/resources/db/migration/V2.10.4__ddl.sql
@@ -0,0 +1,1 @@
+CREATE INDEX idx_dataset_table_task_log_A ON core_datasource_task_log(ds_id, table_name, start_time);
